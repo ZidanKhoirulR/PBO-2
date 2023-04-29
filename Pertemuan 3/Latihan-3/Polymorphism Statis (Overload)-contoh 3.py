e85d5f07b9974bc2b15d92446f082e1a0d90b609
Zidan Khoirul Rizki <126770464+ZidanKhoirulR@users.noreply.github.com>
2023-04-29 16:41:26 +0700

    Add files via upload

---
+++ Pertemuan 3/Latihan-3/Polymorphism Statis (Overload)-contoh 3.py
@@ -0,0 +1,10 @@
+# Nama   : Zidan Khoirul Rizki
+# NIM    : 210511049
+# Kelas  : R2
+# Matkul : Pemrogaman Berorientasi Objek 2
+
+'''Penggunaan fungsi “max” pada berbagai tipe data:'''
+
+print(max(2, 5))  # output: 5
+print(max([1, 2, 3]))  # output: 3
+print(max("Hello"))  # output: "o"
